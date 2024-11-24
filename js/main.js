93ef1e2b9970eaf3bd6be48a25ffd150ebc820f0
Zac Reese <zactreese@gmail.com>
2024-11-23 20:10:19 -0500

    Add toast notification to copy email.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
-/*
- * Auto-generated content from the Brackets New Project extension.
- */
 
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
 /*global $, window, document */
 
-// Simple jQuery event handler
-$(document).ready(function () {
-    "use strict";
-    $("#clickMe").click(function (e) {
-        alert("hello world!");
-    });
-});
+
+function copyEmailAndShowToast() {
+    navigator.clipboard.writeText('zactreese@gmail.com');
+    
+    const toast = document.getElementById('toast');
+    toast.textContent = 'Email Copied! 💌';
+    toast.classList.add('show');
+    
+    setTimeout(function() {
+        toast.classList.remove('show');
+    }, 3000);
+}
